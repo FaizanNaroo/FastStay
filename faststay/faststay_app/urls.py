435c8ca32da37efbc94bc51e582f4183e462d4a9
FaizanNaru <l230729@lhr.nu.edu.pk>
2025-11-12 01:13:35 +0500

    get all users

--- faststay/faststay_app/urls.py
+++ faststay/faststay_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from FastStay.faststay.faststay_app.views import signup_view
+from faststay_app.views.signup_view import signup_view
+from faststay_app.views.get_all_users_view import get_all_users_view 
 urlpatterns={
     path('signup/', signup_view, name='signup'),
+    path('users/all/', get_all_users_view, name='get_all_users'),
 }
-
-
