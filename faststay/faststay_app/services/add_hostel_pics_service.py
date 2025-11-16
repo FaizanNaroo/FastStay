dd35cfd0cc5bf2c7925f283130d9f03c20d61a87
FaizanNaru <l230729@lhr.nu.edu.pk>
2025-11-16 21:49:36 +0500

    add hostel pics case

---
+++ faststay/faststay_app/services/add_hostel_pics_service.py
@@ -0,0 +1,25 @@
+from django.db import connection
+
+class AddHostelPicsService:
+    def _execute_fetch_one(self, function_name, params):
+        try:
+            with connection.cursor() as cursor:
+                placeholders = ','.join(['%s'] * len(params))
+                query = f"SELECT * FROM {function_name}({placeholders})"
+                cursor.execute(query, params)
+                result = cursor.fetchone()
+                print("result ",result)
+            return result
+        except Exception as e:
+            print(f"DB error during execution of {function_name}: {e}")
+            return None
+
+
+    def add_hostel_photo(self, hostel_id: int, photo_link: str):
+       
+        params = [hostel_id, photo_link]
+        result_tuple = self._execute_fetch_one("AddHostelPics", params)
+        if result_tuple is None:
+            return None 
+        
+        return result_tuple[0]
--- faststay/faststay_app/urls.py
+++ faststay/faststay_app/urls.py
@@ -3,9 +3,11 @@ from faststay_app.views.signup_view import SignupView
 from faststay_app.views.get_all_users_view import GetAllUsersView
 from faststay_app.views.login_view import LoginView 
 from faststay_app.views.delete_hostel_details import DeleteHostelDetailsView
+from faststay_app.views.add_hostel_pics_view import AddHostelPics
 urlpatterns={
     path('signup/', SignupView.as_view(), name='signup'),
     path('users/all/', GetAllUsersView.as_view(), name='get_all_users'),
     path('login/', LoginView.as_view(), name='login'),
     path('hosteldetails/delete', DeleteHostelDetailsView.as_view(), name='delete_hostel_details'),
+    path('hostel_pics/add', AddHostelPics.as_view(), name='add_hostel_pics'),
 }
