a331450d612c9d89faac098067d2f9b0978c6321
FaizanNaroo <faizan.naroo@icloud.com>
2026-03-10 16:22:51 +0500

    display only the approved hostels in the recommendation page of student

--- faststay/faststay_app/services/display_service/detail_all_hostels.py
+++ faststay/faststay_app/services/display_service/detail_all_hostels.py
@@ -2,4 +2,5 @@ from faststay_app.services.execute_function import _execute_display_function
 
 class DetailAllHostels:
     def detail_all_hostels(self):
-        return _execute_display_function("DisplayAllHostels")
+        all_hostels = _execute_display_function("DisplayAllHostels")
+        return [h for h in all_hostels if h.get('p_isapproved') is not False]
